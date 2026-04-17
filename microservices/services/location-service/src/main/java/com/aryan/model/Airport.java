2d7ecbf0e81120ac116e6e3897a36608d7cb3038
Aryan <911aaryan@gmail.com>
2026-04-17 19:01:53 +0530

    feat(airport-model): implement Airport entity with JPA annotations and Lombok support

--- microservices/services/location-service/src/main/java/com/aryan/model/Airport.java
+++ microservices/services/location-service/src/main/java/com/aryan/model/Airport.java
@@ -1,4 +1,24 @@
 package com.aryan.model;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
 public class Airport {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(unique = true, nullable = false, length = 3)
+    private String iataCode;
+
+    @Column(nullable = false)
+    private String name;
+
 }
