d802cfd5d97d24089619e885b8869c0b077a8fb8
Aryan <911aaryan@gmail.com>
2026-07-15 12:49:15 +0530

    added airline core services

---
+++ microservices/services/airline-core-service/src/main/java/com/aryan/AirlineCoreServiceApplication.java
@@ -0,0 +1,11 @@
+package com.aryan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirlineCoreServiceApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(AirlineCoreServiceApplication.class, args);
+	}
+}
